f6325ad8deedf6595f41a8fc8fe15654b6b3941e
Kelvin <kelvinchngx@gmail.com>
2018-07-10 15:36:16 -0700

    Update file.

--- Ising_conv3.py
+++ Ising_conv3.py
@@ -48,6 +48,11 @@ flags.DEFINE_string("log_dir",
 
 FLAGS = flags.FLAGS
 
+print(FLAGS.data_dir+"2D10_p_replicas_38_43_50000_%.1d.txt")
+
+import sys
+sys.exit()
+
 HSF = data_reader.insert_file_info(FLAGS.data_dir+"2D10_p_replicas_38_43_50000_%.1d.txt", np.arange(1,2), load_test_data_only=False)
 HSF = HSF.categorize_data()
 
